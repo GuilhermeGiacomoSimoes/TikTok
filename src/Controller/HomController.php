126f1e4b44de22edb94eefd85966aa9e2f383e9c
Guilherme <trintaeoitogc@gmail.com>
2020-05-02 18:26:45 -0300

    config rout to homecontroller

--- src/Controller/HomController.php
+++ src/Controller/HomController.php
@@ -2,9 +2,12 @@
     namespace App\Controller;
 
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\Routing\Annotation\Route;
     
-    class HomeController extends AbstractController
-    {
+    class HomeController extends AbstractController {
+        /**
+         * @Route("/")
+         */
         public function indexAction() {
             return $this->render('index.html.twig');        
         }
