ef7033666b69cc58aeda1456ca10b602314cdb2c
Guilherme <trintaeoitogc@gmail.com>
2020-05-03 12:22:03 -0300

    creating model Funcionario

---
+++ src/Controller/Entity/Funcionario.php
@@ -0,0 +1,41 @@
+<?php 
+namespace App\Entity;
+
+class Funcionario {
+    private $nome; 
+    private $dataDeNascimento;
+    private $dataDeEntrada;
+
+    public function getNome(){
+        return $nome;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getDataDeNascimento(){
+        return $dataDeNascimento;
+    }
+
+    public function setDataDeNascimento($dataDeNascimento){
+        $this->dataDeNascimento = $dataDeNascimento;
+    }
+
+    public function getDataDeEntrada() {
+        return $dataDeEntrada;
+    }
+
+    public function setDataDeEntrada($dataDeEntrada) {
+        $this->dataDeEntrada = $dataDeEntrada;
+    }
+
+    public function getTempoNaEmpresa() {
+        $hoje = new \DateTime();
+        $diferenca = $hoje->diff($this->dataDeEntrada);
+        
+        return $diferenca;
+    }
+}
+
+?>
